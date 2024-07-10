d48f8fbaa6b9266d3aa916f8b468fdd7821c3bc0
YunaKR <126475840+yunaKR-mc@users.noreply.github.com>
2024-07-10 23:43:45 +0900

    CreeperListner.java 업데이트

--- src/main/java/io/github/chaechae/Listner/CreeperListner.java
+++ src/main/java/io/github/chaechae/Listner/CreeperListner.java
@@ -11,9 +11,8 @@ public class CreeperListner implements Listener {
     @EventHandler
     public void onCreeperexplode(EntityExplodeEvent event) {
         if (event.getEntityType() == EntityType.CREEPER) {
-            // TNT 폭발을 막는 로직을 여기에 추가하세요.
-            // 예를 들어, event.setCancelled(true);를 사용하여 폭발을 취소할 수 있습니다.
-            // 또는 다른 방법으로 TNT 폭발을 제어할 수 있습니다.
+            event.setCancelled(true);
+        
         }
     }
 }
